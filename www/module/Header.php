766f875f6d12228c8a2acabba1b03b6c7fd37507
basil <linretin@gmail.com>
2012-07-30 21:42:47 +0800

    update: Header.php

--- www/module/Header.php
+++ www/module/Header.php
@@ -1,11 +1,16 @@
 <?php
 require_once(dirname(__FILE__).'\..\middleware\supper\Modulize.php');
+require_once(dirname(__FILE__).'\SearchBox.php');
 
 class Header extends Modulize {
 	public function __construct() {
 		parent::__construct();
-		//$searchBox= new SearchBox;
-		$this-> assign("SearchBox", "afewwf");
+
+		$SearchBox = new SearchBox();
+		//$SearchBox= SearchBox::getInstance();
+		$smarty= $SearchBox->getPackage('Smarty');
+		$smarty -> assign('logoTip', 'Header');
+		$smarty -> assign('SearchBox', $smarty->display('SearchBox.tpl'));
 	}
 	public function __toString() {
 		return "Class::Header";
