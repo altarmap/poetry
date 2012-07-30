c849a8dba68732475e1f85fb0c4c43f3a75dd678
basil <linretin@gmail.com>
2012-07-30 21:43:24 +0800

    add SearchBox.php

--- www/module/SearchBox.php
+++ www/module/SearchBox.php
@@ -1,30 +1,28 @@
 <?php
- /**
- * Example Application
- */
- //require(dirname(__FILE__).'\..\lib\server\smarty\Smarty.class.php');
- class SearchBox extends Modulize {
-	protected static $instance= null;
+require_once(dirname(__FILE__).'\..\middleware\supper\Modulize.php');
+class SearchBox  extends Modulize {
 	protected static $package= array();
-	protected function __construct() {}
-	public function getPackage($name) {
-		if(self::$package[$name] == null){
+	
+	public function __construct() {
+		parent::__construct();
+		//$this-> assign("SearchBox", "afewwf");
+	}
+	
+	public function getPackage($name) 
+	{
+		if(self::$package[$name] == null)
+		{
 			self::$package[$name]= new $name();
 		}
-		switch($name){
+		switch($name)
+		{
 			case "Smarty":
-				self::$package[$name]-> template_dir= dirname(__FILE__).'\..\templates';
+				self::$package[$name]-> template_dir= dirname(__FILE__).'\..\module_templates';
 				self::$package[$name]-> compile_dir= dirname(__FILE__).'\..\templates_c';
 			break;		
 		}
 		return self::$package[$name];
 	}
-	public static function getInstance() {
-		if(self::$instance == null){
-			self::$instance= new SearchBox;
-		}
-		return self::$instance;
-	}
- }
+}
 
 ?>
