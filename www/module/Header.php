11c5b510260b6e7d5e907f984a12b61715c686fa
wayne <civilwayne@gmail.com>
2012-07-31 01:25:25 +0800

    after merge Header.php

--- www/module/Header.php
+++ www/module/Header.php
@@ -1,12 +1,13 @@
 <?php
 require_once(dirname(__FILE__).'\..\middleware\supper\Modulize.php');
 require_once(dirname(__FILE__).'\..\module\Login.php');
+require_once(dirname(__FILE__).'\..\module\SearchBox.php');
 
 class Header extends Modulize {
 	public function __construct() {
 		parent::__construct();
 		$login= new Login;
-		$SearchBox= new Login;
+		$SearchBox= new SearchBox;
 		$this-> assign("Login", $login-> display("login.tpl"));
 		$this-> assign("SearchBox", $SearchBox-> display("SearchBox.tpl"));
 	}
