674b2f2d9038b652584ed267f5e47959ce103b30
basil <linretin@gmail.com>
2012-07-31 20:10:45 +0800

    delete useless method

--- www/module/SearchBox.php
+++ www/module/SearchBox.php
@@ -1,28 +1,13 @@
 <?php
 require_once(dirname(__FILE__).'\..\middleware\supper\Modulize.php');
 class SearchBox  extends Modulize {
-	protected static $package= array();
 	
 	public function __construct() {
 		parent::__construct();
-		//$this-> assign("SearchBox", "afewwf");
 	}
 	
-	public function getPackage($name) 
-	{
-		if(self::$package[$name] == null)
-		{
-			self::$package[$name]= new $name();
-		}
-		switch($name)
-		{
-			case "Smarty":
-				self::$package[$name]-> template_dir= dirname(__FILE__).'\..\module_templates';
-				self::$package[$name]-> compile_dir= dirname(__FILE__).'\..\templates_c';
-			break;		
-		}
-		return self::$package[$name];
+	public function __toString() {
+		return "Class::SearchBox";
 	}
 }
-
 ?>
