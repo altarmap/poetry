f0be95702d65cdd34fa3a5d38a0f60f7a0d9b687
wayne <civilwayne@gmail.com>
2012-09-30 15:06:09 +0800

    change google3.php

--- www/google/google3.php
+++ www/google/google3.php
@@ -1,9 +1,12 @@
 <?php
+header('Content-type: text/xml');
 require_once(dirname(__FILE__) . "/supper/Search/Search.php");
 require_once(dirname(__FILE__) . "/supper/API/GoogleAPI.php");
 $search = new Search;
-$search-> setKeyword("戴");
-$search-> setAPI(new GoogleAPI);
+$search-> setKeyword($_POST["searchquery"]);
+$google = new GoogleAPI();
+$search-> setAPI($google);
 //echo $search-> getKeyword();
 $search-> query();
+$google-> export();
 ?>
