56d3b65f6ac29849e46801e1d5033f533c1d21ba
wayne <civilwayne@gmail.com>
2012-10-23 00:29:09 +0800

    google/google3.php

--- www/google/google3.php
+++ www/google/google3.php
@@ -1,22 +1,20 @@
 <?php
-header('Content-type: text/xml');
+//header('Content-type: text/xml');
 require_once(dirname(__FILE__) . "/supper/Search/Search.php");
 require_once(dirname(__FILE__) . "/supper/API/GoogleAPI.php");
+require_once(dirname(__FILE__) . "/../middleware/FileManager/FileManager.php");
 $search = new Search;
 $search-> setKeyword($_POST["searchquery"]);
 $google = new GoogleAPI();
 $search-> setAPI($google);
 //echo $search-> getKeyword();
 $search-> query();
-//echo $google-> export() -> asXML();
-$current = time();
-$end = $current + 25;
-$i = true;
-while($i == true) {
-	$now = time();
-	if( $now == $end) {
-		$i = false;
-		echo $google-> export() -> asXML();
-	}
-}	
+$generator = new FileManager($google);
+$generator-> generate();
+$generator = new FileManager($yahoo);
+/*
+$searchInfoXML = $generator -> getXML($lastSearchID);
+$resultXML = $generator -> getXML($searchInfoXML-> resultID);
+$items = $resultXML->item;*/
+
 ?>
