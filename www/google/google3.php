a9756e9f766cfcf2c31b430ee7d0391ff4ea8b8d
wayne <civilwayne@gmail.com>
2012-10-27 14:15:41 +0800

    change google/google3.php

--- www/google/google3.php
+++ www/google/google3.php
@@ -3,6 +3,8 @@
 require_once(dirname(__FILE__) . "/supper/Search/Search.php");
 require_once(dirname(__FILE__) . "/supper/API/GoogleAPI.php");
 require_once(dirname(__FILE__) . "/../middleware/FileManager/FileManager.php");
+require_once(dirname(__FILE__) . "/../middleware/FileManager/SourceCodeManager.php");
+
 $search = new Search;
 $search-> setKeyword($_POST["searchquery"]);
 $google = new GoogleAPI();
@@ -11,10 +13,13 @@ $search-> setAPI($google);
 $search-> query();
 $generator = new FileManager($google);
 $generator-> generate();
-$generator = new FileManager($yahoo);
-/*
-$searchInfoXML = $generator -> getXML($lastSearchID);
-$resultXML = $generator -> getXML($searchInfoXML-> resultID);
-$items = $resultXML->item;*/
+//$generator = new FileManager($yahoo);
+
+$sourceCode= new SourceCodeManager();
+$sourceCode-> generate();
+
+//$searchInfoXML = $generator -> getXML($lastSearchID);
+//$resultXML = $generator -> getXML($searchInfoXML-> resultID);
+//echo $resultXML-> asXML(); 
 
 ?>
