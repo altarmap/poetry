3434c337b7abcaee27700d1ae6bf169842f36f58
wayne <civilwayne@gmail.com>
2012-11-20 15:58:32 +0800

    change folder

--- www/google/supper/Search/ISearch.php
+++
@@ -1,7 +0,0 @@
-<?php
-interface ISearch {
-	function setKeyword ( $keyword = "" );
-	function getKeyword ();
-	function setAPI ( $api = null );
-	function query ();
-}
--- www/google/supper/aDateTime/aDateTime.php
+++
@@ -1,6 +0,0 @@
-<?php
-class aDateTime extends DateTime {
-    public function getTimestamp() {
-         return method_exists('DateTime', 'getTimestamp') ? parent::getTimestamp() : $this->format('U');
-    }
-}
