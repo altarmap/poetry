562f80667b4543c8dda12cdc6ae82f49402b7c34
wayne <civilwayne@gmail.com>
2012-11-24 12:02:36 +0800

    change ModifyHtmlContents.php

--- www/lib/server/simplehtmldom_1_5/example/ModifyHtmlContents.php
+++ www/lib/server/simplehtmldom_1_5/example/ModifyHtmlContents.php
@@ -30,83 +30,85 @@ class ModifyHtmlContents {
 			$dir_files = array_diff(scandir(self::$_dir_folder . "/". $sourceFolder), array('.', '..'));
 			foreach($dir_files as $sourceFile){
 				$filePath= self::$_dir_folder . "/". $sourceFolder . "/" . $sourceFile;
-				$html= file_get_html($filePath);				
-				for ($i=0; $i< count($remove_tag); $i++){
-					foreach ($html-> find($remove_tag[$i]) as $dom){
-						$dom-> outertext= "";
+				$html= file_get_html($filePath);
+				if( gettype($html) == "object" ){
+					for ($i=0; $i< count($remove_tag); $i++){
+						foreach ($html-> find($remove_tag[$i]) as $dom){
+							$dom-> outertext= "";
+						}
+					}				
+					for ($i=0; $i< count($keywords); $i++){
+						foreach ($html-> find("[id*=".$keywords[$i]."]") as $dom) {
+							$dom-> outertext = "";
+						}
+						foreach ($html-> find("[class*=".$keywords[$i]."]") as $dom) {
+							$dom-> outertext = "";
+						}
 					}
-				}				
-				for ($i=0; $i< count($keywords); $i++){
-					foreach ($html-> find("[id*=".$keywords[$i]."]") as $dom) {
-						$dom-> outertext = "";
+					$idDoms = $html-> find('[id*=content]');
+					$classDoms = $html-> find('[class*=content]');
+					$htmlIdArray = array();
+					foreach($idDoms as $item){
+						$tmpStr = preg_replace('/\s+/', '', trim($item-> innertext));
+						if(mb_strlen($tmpStr, 'utf-8') > 20) { 
+							$htmlIdArray[] = $item-> innertext;
+						}
 					}
-					foreach ($html-> find("[class*=".$keywords[$i]."]") as $dom) {
-						$dom-> outertext = "";
+					$htmlClassArray = array();
+					foreach($classDoms as $item){
+						$tmpStr = preg_replace('/\s+/', '', trim($item-> innertext));
+						if(mb_strlen($tmpStr, 'utf-8') > 20) {						
+							$htmlClassArray[] = $item-> innertext;
+						}					
 					}
-				}
-				$idDoms = $html-> find('[id*=content]');
-				$classDoms = $html-> find('[class*=content]');
-				$htmlIdArray = array();
-				foreach($idDoms as $item){
-					$tmpStr = preg_replace('/\s+/', '', trim($item-> innertext));
-					if(mb_strlen($tmpStr, 'utf-8') > 20) { 
-						$htmlIdArray[] = $item-> innertext;
-					}
-				}
-				$htmlClassArray = array();
-				foreach($classDoms as $item){
-					$tmpStr = preg_replace('/\s+/', '', trim($item-> innertext));
-					if(mb_strlen($tmpStr, 'utf-8') > 20) {						
-						$htmlClassArray[] = $item-> innertext;
-					}					
-				}
-				$html-> clear();
-				$htmlString = '';							
-				foreach($htmlIdArray as $item) {
-					$tmpHtmlDOM = str_get_html($item); 
-					$this-> delInsideIdClass($tmpHtmlDOM);
-					$tmpHtmlDOM2 = str_get_html($tmpHtmlDOM-> innertext); 
-					$tmpStr = preg_replace('/\s+/', '', trim($tmpHtmlDOM2-> plaintext));
-					if(mb_strlen($tmpStr, 'utf-8') > 20) {
-						$tmpStr= preg_replace('/[~|!|@|$|%|\^|_|\*|\(|\)|\.|-|\+]+/', '', $tmpStr);
-						$tmpStr= preg_replace('/&nbsp;/', '', $tmpStr);	
+					$html-> clear();
+					$htmlString = '';							
+					foreach($htmlIdArray as $item) {
+						$tmpHtmlDOM = str_get_html($item); 
+						$this-> delInsideIdClass($tmpHtmlDOM);
+						$tmpHtmlDOM2 = str_get_html($tmpHtmlDOM-> innertext); 
+						$tmpStr = preg_replace('/\s+/', '', trim($tmpHtmlDOM2-> plaintext));
 						if(mb_strlen($tmpStr, 'utf-8') > 20) {
-							$htmlString .= '<div id="content">'. $tmpHtmlDOM2-> innertext . '</div>';
+							$tmpStr= preg_replace('/[~|!|@|$|%|\^|_|\*|\(|\)|\.|-|\+]+/', '', $tmpStr);
+							$tmpStr= preg_replace('/&nbsp;/', '', $tmpStr);	
+							if(mb_strlen($tmpStr, 'utf-8') > 20) {
+								$htmlString .= '<div id="content">'. $tmpHtmlDOM2-> innertext . '</div>';
+							}
+							$tmpHtmlDOM2-> clear();
 						}
-						$tmpHtmlDOM2-> clear();
-					}
-					$tmpHtmlDOM-> clear();
-				}				
-				foreach($htmlClassArray as $item) {
-					$tmpHtmlDOM = str_get_html($item); 
-					$this-> delInsideIdClass($tmpHtmlDOM);
-					$tmpHtmlDOM2 = str_get_html($tmpHtmlDOM-> innertext); 
-					$tmpStr = preg_replace('/\s+/', '', trim($tmpHtmlDOM2-> plaintext));
-					if(mb_strlen($tmpStr, 'utf-8') > 20) {
-						$tmpStr= preg_replace('/[~|!|@|$|%|\^|_|\*|\(|\)|\.|-|\+]+/', '', $tmpStr);
-						$tmpStr= preg_replace('/&nbsp;/', '', $tmpStr);	
+						$tmpHtmlDOM-> clear();
+					}				
+					foreach($htmlClassArray as $item) {
+						$tmpHtmlDOM = str_get_html($item); 
+						$this-> delInsideIdClass($tmpHtmlDOM);
+						$tmpHtmlDOM2 = str_get_html($tmpHtmlDOM-> innertext); 
+						$tmpStr = preg_replace('/\s+/', '', trim($tmpHtmlDOM2-> plaintext));
 						if(mb_strlen($tmpStr, 'utf-8') > 20) {
-							$htmlString .= '<div class="content">'. $tmpHtmlDOM2-> innertext . '</div>';
+							$tmpStr= preg_replace('/[~|!|@|$|%|\^|_|\*|\(|\)|\.|-|\+]+/', '', $tmpStr);
+							$tmpStr= preg_replace('/&nbsp;/', '', $tmpStr);	
+							if(mb_strlen($tmpStr, 'utf-8') > 20) {
+								$htmlString .= '<div class="content">'. $tmpHtmlDOM2-> innertext . '</div>';
+							}
+							$tmpHtmlDOM2-> clear();
 						}
-						$tmpHtmlDOM2-> clear();
+						$tmpHtmlDOM-> clear();
 					}
-					$tmpHtmlDOM-> clear();
-				}
-				$exportHtml = str_get_html($htmlString);
-				if($exportHtml) {
-					$html_sha1 = sha1($exportHtml);
-					$folder_name = self::$_repo_folder ."/". substr($html_sha1, 0, 2);
-					$file_name = $folder_name . "/". substr($html_sha1, 2);
-					if(!is_dir($folder_name)){
-						if (!mkdir($folder_name)) {
-							die('Failed to create folders...');
-							return;
+					$exportHtml = str_get_html($htmlString);
+					if($exportHtml) {
+						$html_sha1 = sha1($exportHtml);
+						$folder_name = self::$_repo_folder ."/". substr($html_sha1, 0, 2);
+						$file_name = $folder_name . "/". substr($html_sha1, 2);
+						if(!is_dir($folder_name)){
+							if (!mkdir($folder_name)) {
+								die('Failed to create folders...');
+								return;
+							}
 						}
-					}
-					if(!file_exists($file_name.".html")) {
-						$file= fopen($file_name.".html", "w");
-						fwrite($file, $exportHtml);
-						fclose($file); 
+						if(!file_exists($file_name.".html")) {
+							$file= fopen($file_name.".html", "w");
+							fwrite($file, $exportHtml);
+							fclose($file); 
+						}
 					}
 				}
 			}
